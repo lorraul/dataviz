77c9ffcd91b25d27e8aec270fe883040123134d1
Raul Lorincz <raul.lorincz@bgsoft.ro>
2018-05-25 14:59:50 +0300

    config for heroku

--- gulpfile.js
+++ gulpfile.js
@@ -26,11 +26,6 @@ var watchedBrowserify = watchify(browserify({
         extensions: ['.ts']
     }));
 
-gulp.task('copyHtml', function () {
-    return gulp.src(paths.pages)
-        .pipe(gulp.dest('dist'));
-});
-
 function bundle() {
     return watchedBrowserify
         .bundle()
@@ -44,6 +39,33 @@ function bundle() {
         .pipe(gulp.dest('dist'));
 }
 
+gulp.task('copyHtml', function () {
+    return gulp.src(paths.pages)
+        .pipe(gulp.dest('dist'));
+});
+gulp.task("build", ["copyHtml"], function () {
+    return browserify({
+            basedir: '.',
+            debug: false,
+            entries: ['src/main.ts'],
+            cache: {},
+            packageCache: {}
+        })
+        .plugin(tsify)
+        .transform('babelify', {
+            presets: ['es2015'],
+            extensions: ['.ts']
+        })
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({
+            loadMaps: true
+        }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist'));
+});
 gulp.task("default", ["copyHtml"], bundle);
 watchedBrowserify.on("update", bundle);
 watchedBrowserify.on("log", gutil.log);
