b12ab882f2bc92ba3614bd07e2155c55458c1d29
lorraul <lorraul@gmail.com>
2019-06-24 10:09:58 +0300

    tabletop

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,19 @@ import { httpGetAsync } from "./functions";
 
 import { IGoogleSpreadsheetJson } from './interfaces';
 
+import { Tabletop } from "../node_modules/tabletop/src/tabletop.min.js";
+
 var onSuccess = function(response: IGoogleSpreadsheetJson){
-    document.write(JSON.stringify(response.feed));
+    //document.write(JSON.stringify(response.feed));
+    //console.log(JSON.stringify(response.feed));
 }
 
 httpGetAsync('https://spreadsheets.google.com/feeds/list/1H5WDRHgq6ix2bM2JFLLBN7lsFzJEMbUBGtL_MkNHaxo/1/public/values?alt=json', onSuccess);
+
+
+var tabletop = Tabletop.init({ 
+  key: '1H5WDRHgq6ix2bM2JFLLBN7lsFzJEMbUBGtL_MkNHaxo', 
+  callback: function(data:any, tabletop:any) {
+    console.log(data);
+  }
+})
